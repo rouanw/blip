73d3c0e71f2c7be3c5f8e8f26734089523706b20
Rouan <rmw@thoughtworks.com>
2015-10-31 20:09:36 +0200

    close blip when window is closed

--- app/index.js
+++ app/index.js
@@ -8,13 +8,8 @@ require('crash-reporter').start();
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
+app.on('window-all-closed', function () {
     app.quit();
-  }
 });
 
 // This method will be called when Electron has finished
