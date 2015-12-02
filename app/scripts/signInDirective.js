4cc8fe1b18412efe95b62fac930cd8c95d39c11a
Rouan Wilsenach <myblips@gmail.com>
2015-12-02 05:13:11 +0200

    inline ajax loader

--- app/scripts/signInDirective.js
+++ app/scripts/signInDirective.js
@@ -14,18 +14,17 @@ angular.module('blipApp')
             var authWindow = new BrowserWindow({ width: 800, height: 600, 'node-integration': false, title: 'Sign in to Blip' });
             authWindow.loadUrl('http://localhost:5000/auth/twitter');
             authWindow.webContents.on('did-stop-loading', function () {
-              authWindow.webContents.session.cookies.get({},
-                  function(error, cookies) {
-                    _.forEach(cookies, function (cookie) {
-                      if (cookie.name === 'blipsession') {
-                        $cookies.put(cookie.name, cookie.value);
-                        var $body = angular.element(document.body);
-                        var $rootScope = $body.injector().get('$rootScope');
-                        $rootScope.$digest();
-                        $scope.authenticating = false;
-                        authWindow.close();
-                      }
-                    });
+              authWindow.webContents.session.cookies.get({}, function(error, cookies) {
+                _.forEach(cookies, function (cookie) {
+                  if (cookie.name === 'blipsession') {
+                    $cookies.put(cookie.name, cookie.value);
+                    var $body = angular.element(document.body);
+                    var $rootScope = $body.injector().get('$rootScope');
+                    $rootScope.$digest();
+                    $scope.authenticating = false;
+                    authWindow.close();
+                  }
+                });
               });
             });
           });
