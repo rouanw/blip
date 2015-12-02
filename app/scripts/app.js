c3a6b395918fb79ce7f9f4a0afa3954792cadcf9
Rouan Wilsenach <myblips@gmail.com>
2015-12-02 05:13:11 +0200

    hang on to the rack cookie

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('electron-cookies');
+
 angular
   .module('blipApp', [
     'ngAnimate',
--- app/scripts/signInDirective.js
+++ app/scripts/signInDirective.js
@@ -6,17 +6,38 @@ angular.module('blipApp')
       restrict: 'E',
       templateUrl: 'scripts/sign-in.html',
       controller: function ($scope) {
+        var authWindow;
+        $rootScope.$on('loggedin', function () {
+          console.log('logged in');
+        })
         $scope.signIn = function () {
-          $http.get('http://blip-api-staging.herokuapp.com/auth/github').then(function (result) {
-            if (result.data._id) {
-              Person.set(result.data);
-              $rootScope.authenticated = true;
+          $http.get('http://localhost:5000/person').then(function (result) {
+            if (result.data.uid) {
+                Person.set(result.data);
+                $rootScope.authenticated = true;
             } else {
-              var remote = require('remote');
-              var BrowserWindow = remote.require('browser-window');
-              var authWindow = new BrowserWindow({ width: 800, height: 600, 'node-integration': false });
-              authWindow.openDevTools();
-              authWindow.loadUrl('http://blip-api-staging.herokuapp.com/auth/twitter');
+                var remote = require('remote');
+                var BrowserWindow = remote.require('browser-window');
+                authWindow = new BrowserWindow({ width: 800, height: 600, 'node-integration': false });
+                authWindow.openDevTools();
+                authWindow.loadUrl('http://localhost:5000/auth/twitter');
+                authWindow.webContents.on('did-stop-loading', function () {
+                  authWindow.webContents.session.cookies.get({},
+                      function(error, cookies) {
+                        _.forEach(cookies, function (cookie) {
+                          if (cookie.name === 'rack.session') {
+                            var rackCookie = cookie.name + "=" + cookie.value;
+                            document.cookie = rackCookie;
+                          }
+                        });
+                  });
+                });
+                authWindow.on('closed', function() {
+                  authWindow = null;
+                  var $body = angular.element(document.body);
+                  var $rootScope = $body.injector().get('$rootScope')
+                  $rootScope.$emit('loggedin');
+                });
             }
           });
         };
